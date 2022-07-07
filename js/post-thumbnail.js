be253a251991fee2fc15b208d8b723e48310699c
Mnimaya <mnimayaaa@gmail.com~>
2022-07-07 15:02:20 +0300

    add: post thumbnails

---
+++ js/post-thumbnail.js
@@ -0,0 +1,33 @@
+/**
+ * Содерджимое шаблона миниатюры публикации
+ * @type {DocumentFragment}
+ */
+const thumbnailTemplateContent = document.querySelector('#picture').content;
+
+/**
+ * Создаст DOM-элемент миниатюры публикации
+ * @param {Post} post
+ */
+function createThumbnailElement(post) {
+  const element = thumbnailTemplateContent.querySelector('.picture').cloneNode(true);
+
+  element.querySelector('.picture__img').src = post.url;
+  element.querySelector('.picture__likes').textContent = post.likes;
+  element.querySelector('.picture__comments').textContent  = post.comments.length;
+
+  return element;
+}
+
+/**
+ * Создаст DOM-фрагмент миниатюр публикаций
+ * @param {Post[]} posts
+ */
+function createThumbnailsFragment(posts) {
+  const fragment = new DocumentFragment;
+  fragment.append(...posts.map(createThumbnailElement));
+
+  return fragment;
+}
+
+export default createThumbnailsFragment;
+
