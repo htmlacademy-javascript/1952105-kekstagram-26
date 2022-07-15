5b9103707211f1ea7b0c7333422fa267a51f7d51
Mnimaya <mnimayaaa@gmail.com~>
2022-07-15 12:29:32 +0300

    add: scale control

--- js/upload.js
+++ js/upload.js
@@ -1,4 +1,5 @@
-import createConstraints from './upload-constraints.js';
+import initScaleControl from './upload-scale-control.js';
+import createConstraint from './upload-constraints.js';
 import openModal from './modal.js';
 
 /**
@@ -11,19 +12,60 @@ const formElement = document.querySelector('.img-upload__form');
  * Окно редактирования
  * @type {HTMLDivElement}
  */
-const overlayElement = formElement.querySelector('.img-upload__overlay');
+const modalElement = formElement.querySelector('.img-upload__overlay');
 
-createConstraints(formElement, {
+/**
+ * Редактируемое изображение
+ * @type {HTMLImageElement}
+ */
+const imageElement = modalElement.querySelector('.img-upload__preview img');
+
+/**
+ * Управление масштабом
+ * @type {HTMLFieldSetElement}
+ */
+const scaleControlElement = initScaleControl(
+  modalElement.querySelector('.img-upload__scale')
+);
+
+/**
+ * Методы установки ограничений для хештегов и описаний
+ */
+const constraint = createConstraint(formElement, {
   classTo: 'img-upload__field-wrapper',
   errorTextParent: 'img-upload__field-wrapper',
   errorTextClass: 'text__error'
-})
+});
+
+/**
+ * Меняет масштаб изображения
+ * @param {CustomEvent} event
+ */
+function handleScaleControlUpdate(event) {
+  imageElement.style.transform = `scale(${event.detail.percent / 100})`;
+}
+
+/**
+ * Откроет окно редактирования
+ * @param {Event} event
+ */
+function handleFileNameChange(event) {
+  openModal(modalElement);
+}
+
+// Открытие окна редактирования
+formElement.filename.addEventListener('change', handleFileNameChange);
+
+// Масштабирование изображения
+scaleControlElement.addEventListener('update', handleScaleControlUpdate)
+
+// Ограничения хештегов и описания
+constraint
   .setHashtagsSyntax()
   .setHashtagsMaxItemLength(2)
   .setHashtagsMaxItems(2)
   .setHashtagsRepetitionConstraint()
   .setDescriptionMaxLength(3);
 
-formElement.filename.addEventListener('change', () => {
-  openModal(overlayElement);
-});
+  openModal(modalElement);
+
